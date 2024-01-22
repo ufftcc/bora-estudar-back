095a284debf85390d8f0c0d4b48e53e3d384ce92
ciro germano <ciromrs@gmail.com>
2024-01-22 20:02:56 -0300

    commit inicial

---
+++ src/main/java/com/ufftcc/boraestudar/BoraEstudarBackApplication.java
@@ -0,0 +1,13 @@
+package com.ufftcc.boraestudar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoraEstudarBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoraEstudarBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufftcc/boraestudar/BoraEstudarBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufftcc.boraestudar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoraEstudarBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
