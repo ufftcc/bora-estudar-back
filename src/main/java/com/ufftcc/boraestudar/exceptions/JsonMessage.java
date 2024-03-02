46f0ddc47d4ec27bbd18b7b8d625dca1fe6f4091
ciro germano <ciromrs@gmail.com>
2024-03-01 23:05:11 -0300

    refactor:  JsonMessage class add new constructor

--- src/main/java/com/ufftcc/boraestudar/exceptions/JsonMessage.java
+++ src/main/java/com/ufftcc/boraestudar/exceptions/JsonMessage.java
@@ -6,9 +6,15 @@ public class JsonMessage {
 
     private final String message;
     private final LocalDateTime timestamp;
-    public JsonMessage(String mensagem) {
-        this.message = mensagem;
+
+    public JsonMessage(String message) {
+        this.message = message;
         this.timestamp = LocalDateTime.now();
+    }
+
+    public JsonMessage(String message, LocalDateTime timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
     }
 
     public String getMessage() {
