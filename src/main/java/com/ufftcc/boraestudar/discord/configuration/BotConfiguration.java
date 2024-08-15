7a600ce10a45a646b2218f8d5dfdfc4719bc565b
ciro germano <ciromrs@gmail.com>
2024-08-14 23:25:21 -0300

    feat: add discord

---
+++ src/main/java/com/ufftcc/boraestudar/discord/configuration/BotConfiguration.java
@@ -0,0 +1,45 @@
+package com.ufftcc.boraestudar.discord.configuration;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.gateway.intent.IntentSet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class BotConfiguration {
+
+    @Value("${discord.token}")
+    private String TOKEN;
+
+    @Bean
+    public GatewayDiscordClient gatewayDiscordClient() {
+//        DiscordClient client = DiscordClient.create(TOKEN);
+//        GatewayDiscordClient gateway = client.login().block();
+        GatewayDiscordClient gateway = DiscordClientBuilder.create(TOKEN)
+                .build()
+                .gateway()
+                .setEnabledIntents(IntentSet.all())
+                .login()
+                .block();
+
+        // System.out.println("Registrando os EventListeners . . . ");
+
+
+
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
+                event.getMessage().getChannel().block()
+                        .createMessage("Pong!").block();
+            }
+        });
+
+        return gateway;
+    }
+}
