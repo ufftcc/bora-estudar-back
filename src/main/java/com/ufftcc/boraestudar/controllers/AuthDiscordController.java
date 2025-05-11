e51cc2bebd629b689112db8a04b629f9867106bb
rafael marinho <rafael.marinhodeoliveiraf@gmail.com>
2025-05-11 10:12:34 -0300

    refactor: remove desnecessesary imports

--- src/main/java/com/ufftcc/boraestudar/controllers/AuthDiscordController.java
+++ src/main/java/com/ufftcc/boraestudar/controllers/AuthDiscordController.java
@@ -4,7 +4,6 @@ import com.ufftcc.boraestudar.services.AuthDiscordService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
